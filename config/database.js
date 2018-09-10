ff4eddf314af0091f8515380820b7cb5b5d2f84f
Stanislau Tukhto <s.tukhto.vironit@vironit.ru>
2018-09-10 15:25:29 +0300

    Fixed url to mongoDB server2

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'prodoction') {
 	module.exports = { mongoURI: 'mongodb://stas:vironit1@ds249992.mlab.com:49992/noteted-prod'}
 } else {
-	// module.exports = { mongoURI: 'mongodb://stas:vironit1@ds249992.mlab.com:49992/noteted-prod'}
-	module.exports = { mongoURI: 'mongodb://localhost/noteted-dev'}
+	module.exports = { mongoURI: 'mongodb://stas:vironit1@ds249992.mlab.com:49992/noteted-prod'}
+	// module.exports = { mongoURI: 'mongodb://localhost/noteted-dev'}
 }
